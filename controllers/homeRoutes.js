9569631a1590e3a47d811588de66b46057224085
Derrick Nguyen <ptkn2001@msn.com>
2021-09-21 17:38:33 -0700

    added controllers and views.  Need implementation

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Blog, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async(req, res) => {
+    try {
+        const blogData = await Blog.findAll();
+
+        // Serialize data so the template can read it
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        console.log(blogs);
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
