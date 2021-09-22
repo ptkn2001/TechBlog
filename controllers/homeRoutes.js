0827d09926719e599ffa0f0498c36ddf5e04efca
Derrick Nguyen <ptkn2001@msn.com>
2021-09-22 02:26:41 -0700

    remove comment

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,8 +14,6 @@ router.get('/', async(req, res) => {
         // Serialize data so the template can read it
         const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-        console.log(blogs);
-        // Pass serialized data and session flag into template
         res.render('homepage', {
             blogs,
             logged_in: req.session.logged_in
